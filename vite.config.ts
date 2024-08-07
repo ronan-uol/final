a0c20c53ecb0a01e7227d4ac6226723f13facd1e
ronan-f <rfegan13@gmail.com>
2024-08-06 20:41:21 -0700

    Excluded .html to fix issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   optimizeDeps: {
-    exclude: ["aws-sdk", "nock", "mock-aws-s3"],
+    exclude: ["aws-sdk", "nock", "mock-aws-s3", ".html"],
   },
   plugins: [
     remix({
