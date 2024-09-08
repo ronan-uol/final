65f6d6a07d9c9366d902f4fe21df96d7a487fbaf
ronan-f <rfegan13@gmail.com>
2024-09-08 15:31:25 -0700

    Fixed some typing

--- interfaces/user.ts
+++ interfaces/user.ts
@@ -1,10 +1,16 @@
-
 export interface User {
-    name: string;
-    email: string;
-    password: string;
+  name: string;
+  email: string;
+  password: string;
+  lastSignIn: string;
+  createdOn: string;
+  partnershipId: string;
 }
 
 export interface UserWithId extends Omit<User, "password"> {
-    id: string;
+  id: string;
+}
+
+export interface PartnerWithId extends UserWithId {
+  partnerId: string;
 }
