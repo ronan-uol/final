7ea122c08c8dc7cc7fe57c86549d168e5cc81415
ronan-f <rfegan13@gmail.com>
2024-09-08 14:26:06 -0700

    implemented retrieving partner answers

--- app/routes/quizzes.tsx
+++ app/routes/quizzes.tsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
-import { useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData, useNavigation } from "@remix-run/react";
 import { AuthenticatedLayout } from "~/components/authenticatedLayout";
 import { LoaderFunctionArgs, json, redirect } from "@remix-run/node";
 import { authenticator } from "services/auth/authService.server";
 import { ROUTES } from "~/constants";
-import { getDailyQuiz } from "~/services/quizzes.service";
+import {
+  getDailyQuiz,
+  getPartnerQuizAnswers,
+  submitQuizAnswer,
+} from "~/services/quizzes.service";
+import { useEffect, useRef, useState } from "react";
+import { getPartnerId } from "~/services/user.service";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const user = await authenticator.isAuthenticated(request);
@@ -19,29 +24,63 @@ export async function loader({ request }: LoaderFunctionArgs) {
     return json({ error: "No quizzes found" }, { status: 404 });
   }
 
+  const partnerId = await getPartnerId(user.id);
+
+  if (partnerId) {
+    const partnerAnswers = await getPartnerQuizAnswers(partnerId, quiz.id);
+
+    quiz.questions = quiz.questions.map((question) => {
+      return {
+        ...question,
+        partnerAnswer: partnerAnswers?.[question.id] || null,
+      };
+    });
+  }
+
   return json({ user, quiz });
+}
+
+export async function action({ request }: LoaderFunctionArgs) {
+  const user = await authenticator.isAuthenticated(request);
+
+  if (!user) {
+    return redirect(ROUTES.LOGIN);
+  }
+
+  const formData = await request.formData();
+  const { _action, questionId, answer, quizId } = Object.fromEntries(formData);
+
+  if (_action === "submitAnswer" && questionId && answer) {
+    await submitQuizAnswer({
+      quizId: quizId as string,
+      userId: user.id,
+      questionId: questionId as string,
+      answer: answer as string,
+    });
+    return json({ success: true });
+  }
+
+  return json({ error: "Invalid action" }, { status: 400 });
 }
 
 export default function Quizzes() {
   const { user, quiz } = useLoaderData();
-  const [answers, setAnswers] = useState(quiz.questions.map(() => ""));
   const [revealed, setRevealed] = useState(quiz.questions.map(() => false));
+  const navigation = useNavigation();
+  let submitting = navigation.state === "submitting";
 
-  const handleAnswerChange = (questionIndex, value) => {
-    const updatedAnswers = [...answers];
-    updatedAnswers[questionIndex] = value;
-    setAnswers(updatedAnswers);
-  };
+  const formRef = useRef<HTMLFormElement>(null);
 
-  const handleReveal = (questionIndex) => {
+  useEffect(() => {
+    if (!submitting) {
+      formRef.current?.reset();
+    }
+  }, [submitting]);
+
+  const handleReveal = (questionIndex: number) => {
     const updatedRevealed = [...revealed];
     updatedRevealed[questionIndex] = true;
     setRevealed(updatedRevealed);
-  };
-
-  const handleSubmit = async (questionIndex) => {
-    // Handle your form submission logic here
-    console.log("Submitted answer:", answers[questionIndex]);
   };
 
   return (
@@ -60,36 +99,42 @@ export default function Quizzes() {
                 {quiz.questions.map((question, questionIndex) => (
                   <div
                     key={question.id}
-                    className="rounded-lg shadow-lg p-6 bg-white border-l-4 "
+                    className="rounded-lg shadow-lg p-6 bg-white border-l-4"
                   >
                     <p className="text-lg font-semibold text-blue-800">
                       {question.questionText}
                     </p>
 
-                    <div className="mt-4">
+                    <Form ref={formRef} method="post" className="mt-4">
                       <textarea
-                        value={answers[questionIndex]}
-                        onChange={(e) =>
-                          handleAnswerChange(questionIndex, e.target.value)
-                        }
+                        name="answer"
                         className="w-full p-2 border border-blue-300 rounded-md text-gray-800 bg-blue-50 focus:outline-none focus:ring-2 focus:ring-blue-500"
                         placeholder="Write your answer here..."
                       />
+                      <input
+                        type="hidden"
+                        name="questionId"
+                        value={question.id}
+                      />
+                      <input type="hidden" name="quizId" value={quiz.id} />
                       <div className="mt-4 flex space-x-4">
                         <button
-                          onClick={() => handleSubmit(questionIndex)}
+                          type="submit"
+                          name="_action"
+                          value="submitAnswer"
                           className="bg-green-200 text-gray-800 py-2 px-4 rounded-full font-semibold shadow hover:bg-green-300 transition duration-300"
                         >
                           Submit
                         </button>
                         <button
+                          type="button"
                           onClick={() => handleReveal(questionIndex)}
                           className="bg-blue-600 text-white py-2 px-4 rounded-full font-semibold shadow hover:bg-blue-700 transition duration-300"
                         >
                           Reveal
                         </button>
                       </div>
-                    </div>
+                    </Form>
 
                     {revealed[questionIndex] && (
                       <div className="mt-4 p-4 bg-green-50 border-l-4 border-green-500 rounded-md">
--- app/services/quizzes.service.ts
+++ app/services/quizzes.service.ts
@@ -1,6 +1,56 @@
 import { prisma } from "services/db/db.server";
 import { getAiQuiz } from "./openai.service";
 
+type submitQuizAnswerArgs = {
+  quizId: string;
+  userId: string;
+  questionId: string;
+  answer: string;
+};
+
+export async function submitQuizAnswer({
+  quizId,
+  userId,
+  questionId,
+  answer,
+}: submitQuizAnswerArgs) {
+  try {
+    let completedQuiz = await prisma.completedQuiz.findFirst({
+      where: {
+        userId,
+        quizId,
+      },
+    });
+
+    if (!completedQuiz) {
+      completedQuiz = await prisma.completedQuiz.create({
+        data: {
+          userId,
+          quizId,
+          answers: JSON.stringify({}),
+        },
+      });
+    }
+
+    const answers = JSON.parse(completedQuiz.answers);
+    answers[questionId] = answer;
+
+    const updatedQuiz = await prisma.completedQuiz.update({
+      where: {
+        id: completedQuiz.id,
+      },
+      data: {
+        answers: JSON.stringify(answers),
+      },
+    });
+
+    return { updatedQuiz };
+  } catch (error) {
+    console.error("Error submitting quiz answer:", error);
+    throw new Error("Unable to submit answer");
+  }
+}
+
 export async function getDailyQuiz() {
   const startOfDay = new Date();
   startOfDay.setHours(0, 0, 0, 0);
@@ -26,6 +76,28 @@ export async function getDailyQuiz() {
 
   return quiz;
 }
+
+export async function getPartnerQuizAnswers(userId: string, quizId: string) {
+  const quizzes = await prisma.completedQuiz.findMany({
+    where: {
+      userId,
+    },
+    include: {
+      quiz: true,
+    },
+  });
+
+  console.log({ quizzes });
+
+  const quiz = quizzes.find((quiz) => quiz.quizId === quizId);
+
+  if (!quiz) {
+    return null;
+  }
+
+  return JSON.parse(quiz.answers) as { [key: string]: string };
+}
+
 export async function refreshDailyQuiz() {
   const questions = await getAiQuiz();
 
